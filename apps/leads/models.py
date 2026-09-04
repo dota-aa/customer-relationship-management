3e9df7d0ed66a888adc504747688ad131dc4c799
unknown <alireza808590100@gmail.com>
2026-09-04 16:20:50 +0430

    feat(leads): add Lead model

--- apps/leads/models.py
+++ apps/leads/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class Lead(BaseModel):
+    class LeadSource(models.TextChoices):
+        WEBSITE = 'website', 'Website'
+        REFERRAL = 'referral', 'Referral'
+        SOCIAL = 'social', 'Social Media'
+        EMAIL = 'email', 'Email'
+        OTHER = 'other', 'Other'
+
+    class LeadStatus(models.TextChoices):
+        NEW = 'new', 'New'
+        IN_PROGRESS = 'in_progress', 'In Progress'
+        QUALIFIED = 'qualified', 'Qualified'
+        CONVERTED = 'converted', 'Converted'
+        LOST = 'lost', 'Lost'
+        JUNK = 'junk', 'Junk / Spam'
+
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=11)
+    company = models.CharField(max_length=64)
+
+    source = models.CharField(
+        choices=LeadSource,
+        default=LeadSource.WEBSITE,
+    )
+
+    status = models.CharField(
+        choices=LeadStatus,
+        default=LeadStatus.NEW,
+    )
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
